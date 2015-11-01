8b91b1e797365a8c00b393610c93a3dd0e68ac3a
Adrian Preuß <info@adi-code.de>
2015-11-01 22:06:45 +0100

    update docs

--- docs/api.js
+++ docs/api.js
@@ -2,6 +2,8 @@ YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
         "App",
+        "AppBot",
+        "Broadcast",
         "Exception",
         "Execute",
         "Newsletter",
